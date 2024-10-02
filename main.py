ecd78e56f6ee2e5d5c5a6b8eb080e5399f86c85c
serii burduja <seriiburduja@gmail.com>
2024-10-02 17:00:27 +0300

    feat: main menu report

--- main.py
+++ main.py
@@ -4,4 +4,13 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 menu = Menu()
-choice = askUser(menu)
+work_is_on = True
+while work_is_on:
+    choice = askUser(menu)
+    if choice == "off":
+        work_is_on = False
+    elif choice == "report":
+        CoffeeMaker().report()
+        MoneyMachine().report()
+    else:
+        exit()
